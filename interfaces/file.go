f857489c5deddb1d10e86f0512c652db8c375a22
bframke <goldkilla34@gmail.com>
2019-07-18 00:10:41 +0200

    removed unused dir

--- interfaces/file.go
+++
@@ -1,7 +0,0 @@
-package interfaces
-
-// File is a interface with all available functions of the package file
-type File interface {
-	WriteStringToFile(logLine string)
-	WriteBytesToFile(logline []byte)
-}
